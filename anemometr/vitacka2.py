9c2ab82b930434890e949cfdfa0ef313252bb257
paja <paja@pavlusa.com>
2015-06-25 21:19:48 +0200

    anemometr_2.0

---
+++ anemometr/vitacka2.py
@@ -0,0 +1,9 @@
+from easygui import *
+import easygui
+image = "anem.png"
+msg = "Welcome in program Anemometr"
+choices = ["Yes - I want to measure ", "No"]
+reply = buttonbox(msg, image=image, choices=choices, title="Toz vitajte...")
+hod = 0  # osetreni startu , hod = 0
+
+
